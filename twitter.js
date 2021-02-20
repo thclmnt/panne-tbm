fc1fb579f9276d8bbaff1dce4c5e6bd3102b0e5f
unknown <mail.thomasclement@gmail.com>
2021-02-21 00:20:18 +0100

    Changing twitter stream

--- twitter.js
+++ twitter.js
@@ -1,4 +1,4 @@
-const Twit = require('twit');
+const needle = require('needle');
 const TramId = require('./models/const')
 const Tram = require('./models/tram');
 require('dotenv').config();
@@ -9,6 +9,10 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
         console.log("connected to db")
     })
     .catch((err) => console.log(err));
+
+const token = process.env.BEARER_TOKEN;
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
 
 
 function CREATE_TRAM_REPORT(tramName) {
@@ -24,39 +28,121 @@ function CREATE_TRAM_REPORT(tramName) {
 
 ////////////////////////
 // Twitter Stream
+// FUNCTION FROM: https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/master/Filtered-Stream/filtered_stream.js
 
-// returning tram from twitter id
-function tramIdSolver(id) {
-    if (id==TramId.A) { return 'A' };
-    if (id==TramId.B) { return 'B' };
-    if (id==TramId.C) { return 'C' };
-    if (id==TramId.D) { return 'D' };
-    return -1;
+const rules = [{
+    'value': '("interruptions" OR "interrompu") from:TBM_TramA',
+    'tag': 'A'
+},
+{
+    'value': '("interruptions" OR "interrompu") from:TBM_TramB',
+    'tag': 'B'
+},
+{
+    'value': '("interruptions" OR "interrompu") from:TBM_TramC',
+    'tag': 'C'
+},
+{
+    'value': '("interruptions" OR "interrompu") from:TBM_TramD',
+    'tag': 'D'
+},
+];
+
+async function getAllRules() {
+    const response = await needle('get', rulesURL, {
+        headers: {
+            "authorization": `Bearer ${token}`
+        }
+    })
+    if (response.statusCode !== 200) {
+        throw new Error(response.body);
+    }
+    return (response.body);
 }
 
-// search term on twitter
-const SEARCH_TRAM = `"#TBMTram" AND ("interrompu" OR "interruptions")`;  
-// logging to twitter API using key in .env file
-let client = new Twit({
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-});
-
-// stream API connection
-let stream = client.stream('statuses/filter', { track: SEARCH_TRAM });
-
-// create new tram report on tweet
-stream.on('tweet', function (tweet) {
-    let idTweet = tweet.user.id
-    let idTram = tramIdSolver(idTweet);
-    if (idTram != -1){
-        CREATE_TRAM_REPORT(idTram);
+async function deleteAllRules(rules) {
+    if (!Array.isArray(rules.data)) {
+        return null;
     }
-    console.log(tweet.user.id)
-});
+    const ids = rules.data.map(rule => rule.id);
+    const data = {
+        "delete": {
+            "ids": ids
+        }
+    }
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    })
+    if (response.statusCode !== 200) {
+        throw new Error(response.body);
+    }
+    return (response.body);
+}
 
-stream.on('error', function (error) {
-    console.log(error);
-})
+
+async function setRules() {
+    const data = {
+        "add": rules
+    }
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    })
+    if (response.statusCode !== 201) {
+        throw new Error(response.body);
+    }
+    return (response.body);
+}
+
+
+function streamConnect() {
+    const stream = needle.get(streamURL, {
+        headers: {
+            "User-Agent": "v2FilterStreamJS",
+            "Authorization": `Bearer ${token}`
+        },
+        timeout: 20000
+    });
+    stream.on('data', data => {
+        try {
+            const json = JSON.parse(data);
+            let tram = json['matching_rules'][0].tag;
+            CREATE_TRAM_REPORT(tram);
+        } catch (e) {
+        }
+    }).on('error', error => {
+        if (error.code === 'ETIMEDOUT') {
+            stream.emit('timeout');
+        }
+    });
+    return stream;
+}
+
+
+(async () => {
+    let currentRules;
+    try {
+        currentRules = await getAllRules();
+        await deleteAllRules(currentRules);
+        await setRules();
+    } catch (e) {
+        console.error(e);
+        process.exit(-1);
+    }
+    const filteredStream = streamConnect();
+    let timeout = 0;
+    filteredStream.on('timeout', () => {
+        console.warn('A connection error occurred. Reconnecting…');
+        setTimeout(() => {
+            timeout++;
+            streamConnect();
+        }, 2 ** timeout);
+        streamConnect();
+    })
+
+})();
